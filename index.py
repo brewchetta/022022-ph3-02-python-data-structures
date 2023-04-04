247592f5fe23ef7cc3c4866fa50d3ce922bcf7f1
brewchetta <chett.tiller@gmail.com>
2023-04-04 12:09:59 -0400

    notes from lecture

--- index.py
+++ index.py
@@ -1,12 +1,37 @@
-my_cats = ["octavia", "ursula", "sundae", "luna"]
+my_cats = ["octavia", "ursula", "sundae", "luna", "luna"]
+
+# js ==> .push('some new value')
+# py ==> .append('some new value')
+# js & py ==> .pop() --> remove a value from the end
+# py ==> .insert(2, 'some value to insert at index 2')
+
+# py ==> .remove('value to remove')
+# py ==> del( list_name[index] )
+
+# py ==> len( list ) --> gets the length of the list
+
+# js ==> array.slice(0,2)
+# py ==> list[0:2]
+
+# py ==> list.sort() --> sorts alphanumerically (destructive)
+# py ==> sorted(list) --> non-destructively sorts
+
+my_list_with_curlies = {1,2,3,4,5,1,2,3,4,5}
+# evaluates to {1,2,3,4,5}
 
 continents = ("north america", "south america", "africa", "europe", "asia")
 
+# py ==> a tuple is immutable!
+
 one_to_nine = range(10)
+
+# py ==> makes a range of numbers from 0-9
 
 raccoon_names = { "rocky", "rocket", "ralph", "rick" }
 
-animal_dictionary = {
+# py ==> a set doesn't allow for duplicates, having two "rocky"s in here would filter one of them out
+
+animals = {
     "aardvark": "A fancy animal with a big snout",
     "bumblebee": "Always ready to rumble",
     "cat": "Lazy, somehow so soft and sharp at the same time",
