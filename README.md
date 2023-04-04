de08f8fdec0f39e11f827a499b15a2d7319aecb4
brewchetta <chett.tiller@gmail.com>
2023-04-04 10:37:53 -0400

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Python Data Structures
+
+## Learning Goals
+
+- Lists and their methods
+
+- List comprehensions and list generators
+
+- Tuples and how they differ from lists
+
+- Ranges as an easy way to build a sequence of numbers
+
+- Sets as a list-like object of unique values
+
+- Dictionaries and their methods
---
+++ index.py
@@ -0,0 +1,16 @@
+my_cats = ["octavia", "ursula", "sundae", "luna"]
+
+continents = ("north america", "south america", "africa", "europe", "asia")
+
+one_to_nine = range(10)
+
+raccoon_names = { "rocky", "rocket", "ralph", "rick" }
+
+animal_dictionary = {
+    "aardvark": "A fancy animal with a big snout",
+    "bumblebee": "Always ready to rumble",
+    "cat": "Lazy, somehow so soft and sharp at the same time",
+    "dog": "Loyal to the end",
+    "eagle": "Some say they're hawk eyed",
+    "fish": "There are lots of them in the sea"
+}
